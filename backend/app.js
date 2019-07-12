e05aa0a18aaf15576f3b9c29ad152ac785710a5e
jbj616 <jbj616@gmail.com>
2019-07-12 10:57:51 +0900

    test

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,12 @@ const fs = require("fs");
 const app = express();
 
 //설정
-app.use(express.static("../frontend"));
+app.use(express.static("./frontend"));
 
 //get 요청
 app.get("/", (req, res) => {
   console.log("get(/)요청 실행");
-
-  fs.readFile("../frontend/index.html", (err, data) => {
+  fs.readFile("./frontend/index.html", (err, data) => {
     if (err) throw err;
 
     res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
@@ -21,9 +20,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/plan", (req, res) => {
-  console.log("post(/plan)요청 실행");
+  console.log("get(/plan)요청 실행");
 
-  fs.readFile("../frontend/plan.html", (err, data) => {
+  fs.readFile("./frontend/plan.html", (err, data) => {
     if (err) throw err;
 
     res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
@@ -31,15 +30,9 @@ app.get("/plan", (req, res) => {
   });
 });
 
-app.get("/process/plan", (req, res) => {
-  console.log("get(/process/plan)실행");
-
-  fs.readFile("../frontend/polylineEx.html", (err, data) => {
-    if (err) throw err;
-
-    res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
-    res.end(data);
-  });
+//error
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>요청하신 페이지는 없어요.</h1>");
 });
 
 //서버 실행
