4863e4983382438dea115e0f8335cc9d5325519e
jbj616 <jbj616@gmail.com>
2019-07-12 14:19:18 +0900

    hosting test

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,16 @@ app.get("/plan", (req, res) => {
   });
 });
 
+app.get("/process/plan", (req, res) => {
+  console.log("get(/process/plan)요청 실행");
+
+  fs.readFile("./frontend/polylineEX.html", (err, data) => {
+    if (err) throw err;
+
+    res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+    res.end(data);
+  });
+});
 //error
 app.all("*", (req, res) => {
   res.status(404).send("<h1>요청하신 페이지는 없어요.</h1>");
