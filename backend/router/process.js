176c212ab11a505094bba0eaf1555b6f80ececde
jbj616 <jbj616@gmail.com>
2019-07-26 15:17:22 +0900

    fds

--- backend/router/process.js
+++ backend/router/process.js
@@ -76,8 +76,6 @@ module.exports = (app, database) => {
     const apihtml = require("../util/schedule.js");
     const dateUtil = require("../util/date.js");
 
-    console.log("Test");
-
     let departureLocation = req.body.departureLocation;
     let departureStation = req.body.departureStation;
     let arrivalLocation = req.body.arrivalLocation;
